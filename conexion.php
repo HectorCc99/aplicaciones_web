bbc87d842367159af06ee962046f3902a49ed31a
HectorCc99 <83419321+HectorCc99@users.noreply.github.com>
2021-06-01 12:40:50 -0500

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,10 @@
+
+<?php
+$mysqli = new mysqli("localhost", "root", "", "sistemaweb");
+if ($mysqli->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}else{
+    echo "Conexion Exitosa";
+}
+
+?>
